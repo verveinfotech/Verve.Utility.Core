483015e162cceba50689daf6e30e54e32259ceee
azizkap21 <aziz@azizweb.com>
2020-05-17 23:30:32 +0100

    Added new project for result usitlity

---
+++ Verve.Utility.Core.ContractResult/Verve.Utility.Core.ContractResult/ApiResponse.cs
@@ -0,0 +1,36 @@
+﻿using JetBrains.Annotations;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace Verve.Utility.Core.ContractResult
+{
+    [UsedImplicitly]
+    public static class ResultExtension 
+    {
+        [UsedImplicitly]
+        public static IActionResult ToJsonContentResult(this Result result)
+        {
+            return new ContentResult
+            {
+                Content = JsonConvert.SerializeObject(result),
+                ContentType = "application/json",
+                StatusCode = GetStatusCode(result)
+            };
+        }
+
+        private static int GetStatusCode(Result result)
+        {
+            if (result.ReasonCode == ReasonCode.UnknownError)
+            {
+                return 500;
+            }
+
+            if ((int)result.ReasonCode > 599)
+            {
+                return 500;
+            }
+
+            return (int) result.ReasonCode;
+        }
+    }
+}
---
+++ Verve.Utility.Core.ContractResult/Verve.Utility.Core.ContractResult/EntityResult.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using JetBrains.Annotations;
+
+namespace Verve.Utility.Core.ContractResult
+{
+    public class Result<TEntity> : Result
+        where TEntity : class, new()
+    {
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="success"></param>
+        /// <param name="reason"></param>
+        /// <param name="userFriendlyMessage">A user friendly message to show end users about the result.</param>
+        /// <param name="developerFriendlyMessage">A message that might be helpful for developers to find out about the result.</param>
+        /// <param name="entity"></param>
+        [UsedImplicitly]
+        public Result(bool success, ReasonCode reason, string? userFriendlyMessage, string? developerFriendlyMessage, [CanBeNull][AllowNull] TEntity entity)
+            : base(success, reason, userFriendlyMessage, developerFriendlyMessage)
+        {
+            if (success && entity == null && reason != ReasonCode.NoContent)
+            {
+                throw new ArgumentException($"Parameter {nameof(entity)} must have a value when {nameof(success)} is 'true' and {nameof(reason)} is not '{nameof(ReasonCode.NoContent)}'.");
+            }
+
+            if (success &&
+                reason != ReasonCode.Accepted &&
+                reason != ReasonCode.NoContent &&
+                reason != ReasonCode.Success)
+            {
+                throw new ArgumentException(
+                    $"Parameter '{nameof(success)}' is set to 'true' but the value of the '{reason}' is not one of the values: " +
+                    $"'{nameof(ReasonCode.Accepted)}', '{nameof(ReasonCode.NoContent)}', '{nameof(ReasonCode.Success)}'.");
+            }
+
+            Entity = entity;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="success"></param>
+        /// <param name="reason"></param>
+        /// <param name="userFriendlyMessage">A user friendly message to show end users about the result.</param>
+        /// <param name="developerFriendlyMessage">A message that might be helpful for developers to find out about the result.</param>
+        /// <param name="entity"></param>
+        [UsedImplicitly]
+        public Result(bool success, ReasonCode reason, string? userFriendlyMessage, string? developerFriendlyMessage)
+            : this(success, reason, userFriendlyMessage, developerFriendlyMessage, default(TEntity))
+        {
+        }
+
+        public Result(bool success, TEntity entity, ReasonCode reasonCode)
+        {
+            Succeeded = success;
+            Entity = entity;
+            ReasonCode = reasonCode;
+        }
+
+        [UsedImplicitly]
+        [JetBrains.Annotations.NotNull]
+        public virtual Result Success(TEntity entity)
+        {
+            return new Result<TEntity>(true, entity, ReasonCode.Success);
+        }
+
+        public Result From(Result other)
+        {
+            if (other == null)
+            {
+                return new Result<TEntity>(false, ReasonCode.UnknownError, "An error occurred.", "Other result was null.");
+            }
+
+            var otherContent = default(TEntity)!;
+            var typeOfOther = other.GetType();
+
+            if (other.Succeeded)
+            {
+                if (typeOfOther.IsGenericType && typeOfOther.GenericTypeArguments[0].IsSubclassOf(typeof(TEntity)))
+                {
+                    var property = typeOfOther.GetProperty(nameof(Entity));
+                    otherContent = (TEntity)property?.GetValue(other)!;
+                }
+                else
+                {
+                    throw new ArgumentException($"Type of '{nameof(other)}.{nameof(Entity)}' is not assignable to '{typeof(TEntity)}'.");
+                }
+            }
+
+            return new Result<TEntity>(other.Succeeded, other.ReasonCode, other.ErrorMessage, other.ErrorMessage, otherContent);
+        }
+
+        /// <summary>
+        /// Gets or sets content of the result.
+        /// </summary>
+        [MaybeNull]
+        public TEntity Entity { get; protected set; }
+
+        
+    }
+}
