ce0328b870beccf4c2d8d4f2678aa03c75890682
azizkap21 <aziz@azizweb.com>
2020-06-08 20:48:51 +0100

    Enhancements

---
+++ Verve.Utility.Core.ContractResult/Verve.Utility.Core.ContractResult.Tests/ContentResultTest.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Xunit;
+
+namespace Verve.Utility.Core.ContractResult.Tests
+{
+    public class ContentResultTest
+    {
+        [Fact]
+        public void ReturnResultWithDefaultContent()
+        {
+            var result = new ContentResult<string>(true, ReasonCode.Success, null, null, string.Empty);
+
+            Assert.NotNull(result.Content);
+            Assert.Equal(string.Empty, result.Content);
+
+        }
+
+        [Fact]
+        public void ReturnResultWithDefaultContentFromOtherFailure()
+        {
+            var result = new ContentResult<TestPerson>(false, ReasonCode.BadRequest, "Bad Request", "Test Failure", default!);
+
+            var stringResult = ContentResult<string>.FailedFromOtherFailed(result);
+
+            Assert.NotNull(stringResult);
+            
+            Assert.Equal(default, stringResult.Content);
+        }
+
+        [Fact]
+        public void ReturnValueTypeResultWithDefaultContentFromOtherFailure()
+        {
+            var result = new ContentResult<TestPerson>(false, ReasonCode.BadRequest, "Bad Request", "Test Failure", default!);
+
+            var stringResult = ContentResult<int>.FailedFromOtherFailed(result);
+
+            Assert.NotNull( stringResult );
+
+            Assert.Equal( default, stringResult.Content );
+        }
+
+        [Fact]
+        public void ReturnNotNullContentForRefType()
+        {
+            var result = ContentResult<string>.Failure("Test error", "test detail error", ReasonCode.BadRequest);
+
+            var objectResult = ContentResult<TestPerson>.FailedFromOtherFailed(result);
+            
+            Assert.NotNull(objectResult);
+            Assert.NotNull(objectResult.Content);
+            Assert.Equal(Guid.Empty, objectResult.Content.Id);
+        }
+
+    }
+}
